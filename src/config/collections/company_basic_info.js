942c452fc1bb7926be092100f3f0e65904d1b29f
brunovbcz <brunovbcz9@gmail.com>
2026-01-30 16:17:25 -0300

    feat: add about page, initial page, and login page with corresponding styles and functionality
    - Created aboutPage.html with content about the Ecoleta platform.
    
    - Developed initialPage.html as the landing page with company listings and filters.
    
    - Implemented loginPage.html for user authentication.
    
    - Added styles for about, initial, and login pages to enhance UI.
    
    - Introduced company_basic_info.js for handling company data operations (CRUD).
    
    - Established database.js for MongoDB connection management.
    
    - Set up server.js to handle routing and API endpoints for company data.
    
    - Included a search feature with a magnifying glass icon for better user experience.

---
+++ src/config/collections/company_basic_info.js
@@ -0,0 +1,83 @@
+const { connectToDb, getDb, ObjectId } = require('../database.js');
+
+const encontrarEmpresa = async () => {
+  try {
+    await connectToDb();
+    const bd = getDb();
+    console.log(getDb())
+    const collection_empresas = bd.collection("empresas");
+    const encontrar = await collection_empresas.find().toArray();
+
+    if (encontrar.length === 0) {
+      console.log("Nenhum documento foi encontrado.");
+      return null; 
+    }
+
+    console.log(encontrar)
+    return encontrar;
+
+  } catch (err) {
+    console.error("Erro ao encontrar empresa:", err.message);
+    throw err;
+  }
+};
+
+const criarDadosDeIdentificacao = async (nome_empresa, cnpj, razao_social, logo, descricao) => {
+  try {
+    await connectToDb();
+    const bd = getDb();
+    const collection_empresas = bd.collection("empresas");
+  
+    const novosDados = {
+      nome_empresa: nome_empresa,
+      cnpj: cnpj,
+      razao_social: razao_social,
+      logo: logo,
+      descricao: descricao
+    }
+    const resultado = await collection_empresas.insertOne(novosDados);
+    return novosDados;
+  } catch (err) {
+    console.error("Erro ao inserir dados de identificação:", err.message);
+    throw err;
+  }
+};
+
+const excluirDadosDeIdentificacao = async (id) => {
+  try {
+    await connectToDb();
+    const bd = getDb();
+    const collection_empresas = bd.collection("empresas");
+
+    const resultado = await collection_empresas.deleteOne({ _id: new ObjectId(id) });
+    return resultado.deletedCount > 0;
+  } catch (err) {
+    console.error("Erro ao excluir dados de identificação da empresa:", err.message);
+    throw err;
+  }
+};
+
+const editarDadosDeIdentificacao = async (id, updatedData) => {
+  try {
+    await connectToDb();
+    const bd = getDb();
+    const collection_empresas = bd.collection("empresas");
+
+    const resultado = await collection_empresas.updateOne(
+      { _id: new ObjectId(id) },
+      { $set: updatedData }
+    );
+    
+    return resultado.modifiedCount > 0;
+  } catch (err) {
+    console.error("Erro ao editar dados de identificação da empresa:", err.message);
+    throw err;
+  }
+}
+
+module.exports = {
+  encontrarEmpresa,
+  criarDadosDeIdentificacao,
+  excluirDadosDeIdentificacao,
+  editarDadosDeIdentificacao
+};
---
+++ src/config/database.js
@@ -0,0 +1,40 @@
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const path = require('path');
+require('dotenv').config(); 
+const uri = process.env.MONGO_URI;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+  connectTimeoutMS: 100000, 
+  socketTimeoutMS: 450000,
+  maxPoolSize: 10,
+  minPoolSize: 2 
+});
+
+let bd;
+
+const connectToDb = async () => {  
+  try {
+    await client.connect();
+    bd = client.db("sistema-ecoleta");
+  } catch (err) {
+    console.error("Erro ao conectar ao MongoDB:", err);
+    throw err; 
+  }
+};
+
+const closeConnection = async () => {
+  await client.close();
+};
+
+module.exports = {
+  connectToDb,
+  closeConnection,
+  ObjectId,
+  client,
+  getDb: () => bd
+};
