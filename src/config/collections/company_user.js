9e50b291cd222611ca4d2e1b996c0c0bcf266b6b
brunovbcz <brunovbcz9@gmail.com>
2026-02-05 17:07:57 -0300

    feat: add CORS support and user retrieval functionality
    - Added "cors" dependency to package.json and package-lock.json for handling CORS in the application.
    - Updated loginPage.html to change the registration link to point to the correct route.
    - Implemented findUser function in company_user.js to retrieve user details by ID with error handling.
    - Integrated findUser function into server.js to allow fetching user data via a new endpoint.
    - Added a new sign-in page (signInPage.html) with a multi-step registration form.
    - Created signIn.js to handle the multi-step form logic and dynamic point of collection selection.
    - Added styles for the new sign-in page in signIn.css.

--- src/config/collections/company_user.js
+++ src/config/collections/company_user.js
@@ -1,6 +1,37 @@
 const { connectToDb, getDb, ObjectId } = require('../database.js');
 const collection = 'company_users';
 
+const findUser = async (id) => {
+  try {
+    await connectToDb();
+    const bd = getDb();
+    const collection_users = bd.collection(collection);
+
+    if (!ObjectId.isValid(id)) {
+      console.log("ID fornecido é inválido.");
+      return null;
+    }
+    try {
+      objectId = ObjectId.createFromHexString(id);
+    } catch (error) {
+      objectId = null;
+    }
+    const user = await collection_users.findOne({ _id: objectId});
+
+    if (!user) {
+      console.log("Nenhum documento foi encontrado.");
+      return null;
+    }
+
+    console.log("Usuário encontrado:", user);
+    return user;
+
+  } catch (err) {
+    console.error("Erro ao encontrar usuário:", err.message);
+    throw err;
+  }
+}
+
 const registerCompany = async (email, password) => {
   try {
     await connectToDb();
@@ -49,6 +80,7 @@ const getUsers = async () => {
 
 
 module.exports = {
-    registerCompany,
-    getUsers
+  findUser,
+  registerCompany,
+  getUsers
 }
